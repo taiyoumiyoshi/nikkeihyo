84e4d58b5dd8371bee495eeb09a4f8f474fd2f78
taiyoumiyoshi <taiyou.miyoshicity@gmail.com>
2026-06-09 12:53:07 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// ============================================================
+// 整骨鍼灸院たいよう 日計表システム - 設定ファイル
+// GAS URLが変わったときはここだけ変更してください
+// ============================================================
+const CONFIG = {
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbz_zymlQzLyblb235nvMCNeTEb9UrptRg1BfA2vyx8MZSUWf3F42BHRP8VXnKpCaRR6/exec'
+};
